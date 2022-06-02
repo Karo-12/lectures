9952efb563bcf32b2bdc85030396b9241678c580
Sergiy <sergey.morenets@gmail.com>
2022-06-02 20:35:02 +0300

    Lecture 1. Calculator definition

---
+++ src/main/java/capgemini/java8/Calculator.java
@@ -0,0 +1,14 @@
+package capgemini.java8;
+
+public class Calculator {
+
+    public int sum(int x, int y) {
+        return x + y;
+    }
+
+    public int minus(int x, int y) {
+        return x - y;
+    }
+
+    //TODO add multiple/divide
+}
