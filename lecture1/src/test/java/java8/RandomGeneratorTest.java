25c7e1044d54149d6452479df18c15e9e1468859
Karolina <200626@student.pwr.edu.pl>
2022-06-06 00:21:27 +0200

    add unit tests checking if method is returning random number

--- lecture1/src/test/java/java8/RandomGeneratorTest.java
+++ lecture1/src/test/java/java8/RandomGeneratorTest.java
@@ -1,4 +1,27 @@
 package java8;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
 public class RandomGeneratorTest {
+
+    RandomGenerator randomGenerator;
+
+    @BeforeEach
+    void setup() {
+        randomGenerator = new RandomGenerator();
+    }
+
+    @Test
+    @DisplayName("Check if number is random")
+    void generate_IfTwoNumbersAreNotTheSame(){
+        int numberOne = randomGenerator.generate(1000);
+        int numberTwo = randomGenerator.generate(1000);
+        assertNotEquals(numberOne, numberTwo);
+    }
+
 }
