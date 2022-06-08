e2ae4155dfa289c683c1ef6f21c6aa3a29c6b8b6
Karolina <200626@student.pwr.edu.pl>
2022-06-08 22:21:54 +0200

    delete imports

--- lecture1/src/test/java/java8/RandomGeneratorTest.java
+++ lecture1/src/test/java/java8/RandomGeneratorTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 
