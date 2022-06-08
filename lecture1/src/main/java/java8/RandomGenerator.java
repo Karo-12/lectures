b071727f40eb5d314a88a2dcf10990230c38ee12
Karolina <200626@student.pwr.edu.pl>
2022-06-08 22:20:39 +0200

    change unit test for random numbers generator

--- lecture1/src/main/java/java8/RandomGenerator.java
+++ lecture1/src/main/java/java8/RandomGenerator.java
@@ -8,9 +8,12 @@ import java.util.Random;
 //that check its functionality and especially that the class returns random values
 public class RandomGenerator {
 
+    private int counter = 0;
     private final Random random = new Random();
 
     public int generate(int max) {
+        //if(counter <= max) counter++;
+        //return counter;
         return random.nextInt(max);
     }
 }
--- lecture1/src/test/java/java8/RandomGeneratorTest.java
+++ lecture1/src/test/java/java8/RandomGeneratorTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class RandomGeneratorTest {
@@ -18,10 +21,21 @@ public class RandomGeneratorTest {
 
     @Test
     @DisplayName("Check if number is random")
-    void generate_IfTwoNumbersAreNotTheSame(){
-        int numberOne = randomGenerator.generate(1000);
-        int numberTwo = randomGenerator.generate(1000);
-        assertNotEquals(numberOne, numberTwo);
+    void generate_IfNumbersAreGeneratedRandomly_ResultIsPositive() {
+        final int maxValue = 500;
+        int [] randomArray = new int[maxValue];
+        for(int i = 0; i < maxValue ; i++){
+            randomArray[i] = randomGenerator.generate(maxValue);
+        }
+        int counterSame = 0;
+        int counterDifference = 0;
+        int difference = 0;
+        for(int i = 0; i < maxValue-1; i++) {
+            if(randomArray[i] == randomArray[i+1]) counterSame++;
+            if(difference == Math.abs(randomArray[i] - randomArray[i+1])) counterDifference++;
+            difference = Math.abs(randomArray[i] - randomArray[i+1]);
+        }
+        assertTrue(counterSame <(maxValue*0.01));
+        assertTrue(counterDifference < (maxValue*0.01));
     }
-
 }
