7152a0914d6fd6228d522fd496ee2c913c726641
Karolina <200626@student.pwr.edu.pl>
2022-06-09 09:40:30 +0200

    change variables names

--- lecture1/src/test/java/java8/RandomGeneratorTest.java
+++ lecture1/src/test/java/java8/RandomGeneratorTest.java
@@ -20,25 +20,25 @@ public class RandomGeneratorTest {
     @DisplayName("Check if number is random")
     void generate_IfNumbersAreGeneratedRandomly_ResultIsPositive() {
         final int maxValue = 500;
+        int countSameNumbers = 0;
+        int countDifference = 0;
+        int previousDifference = 0;
+        int previousNumber = 0;
+        int countSumPreviousNumbers=0;
         int [] randomArray = new int[maxValue];
         for(int i = 0; i < maxValue ; i++){
             randomArray[i] = randomGenerator.generate(maxValue);
         }
-        int counterSame = 0;
-        int counterDifference = 0;
-        int previousDifference = 0;
-        int previousNumber = 0;
-        int previousSumCounter=0;
         for(int i = 0; i < maxValue-1; i++) {
             int difference = Math.abs(randomArray[i] - randomArray[i+1]);
-            if(randomArray[i] == randomArray[i+1]) counterSame++;
-            else if(previousDifference == difference) counterDifference++;
-            else if(previousNumber + randomArray[i] == randomArray[i+1]) previousSumCounter++;
+            if(randomArray[i] == randomArray[i+1]) countSameNumbers++;
+            else if(previousDifference == difference) countDifference++;
+            else if(previousNumber + randomArray[i] == randomArray[i+1]) countSumPreviousNumbers++;
             previousDifference = difference;
             previousNumber = randomArray[i];
         }
-        assertTrue(counterSame <(maxValue*0.01));
-        assertTrue(counterDifference < (maxValue*0.01));
-        assertTrue(previousSumCounter < (maxValue*0.01));
+        assertTrue(countSameNumbers <(maxValue*0.01));
+        assertTrue(countDifference < (maxValue*0.01));
+        assertTrue(countSumPreviousNumbers < (maxValue*0.01));
     }
 }
